94681d6ac7d3678a91b52c3769a50f1627cc7539
KronosFPV <137655816+KronosFPV@users.noreply.github.com>
2025-02-12 09:27:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -105,4 +105,6 @@ function calculateTotal() {
 
     // Anzeigen der Steuern und Versandkosten
     document.getElementById('totalPrice').textContent = total.toFixed(2);
-    document.getElementById('shippingLabel').textContent = `Versandkosten:
+    document.getElementById('shippingLabel').textContent = `Versandkosten: ${totalShipping.toFixed(2)} ${currency}`;
+    document.getElementById('taxLabel').textContent = `MwSt.: ${totalTax.toFixed(2)} ${currency}`;
+}
